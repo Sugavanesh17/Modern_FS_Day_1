a611a881a4c91a67c311f18f1f0ffce919e6c384
Sugavanesh17 <sugavaneshwaran.s2023cse@sece.ac.in>
2025-07-03 10:17:05 +0530

    Day 2

---
+++ Day2/script.js
@@ -0,0 +1,28 @@
+// console.log("Hello World from script.js");
+// var a=10;
+// var a=20;
+// const b=10;
+// var c=10;
+// console.log(a+b)
+
+// let pro_key="1234567890";
+//  pro_key="0987654321";
+//  console.log(pro_key);
+
+//  const a=12;
+//  a=20; // This will throw an error because 'a' is a constant
+// console.log(a);
+
+// for(let i=0;i<10;i++){
+//     console.log(i);
+// }
+let i=1;
+do{
+    console.log(i);
+    i++;
+}while(i<=10);
+
+while(i<=10){
+    console.log(i);
+    i++;
+}
